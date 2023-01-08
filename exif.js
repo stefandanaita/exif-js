ef79048c72b98b273af30f8ccfb054c29c832a8d
Stefan Danaita <me@dsa.io>
2023-01-08 23:56:04 +0200

    Update exif.js

--- exif.js
+++ exif.js
@@ -738,7 +738,7 @@
 
     function getStringFromDB(buffer, start, length) {
         var outstr = "";
-        for (var n = start; n < start+length; n++) {
+        for (let n = start; n < start+length; n++) {
             outstr += String.fromCharCode(buffer.getUint8(n));
         }
         return outstr;
